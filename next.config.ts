ed6a1bd30018453b74b56086f456f8afaba3bd56
NGUETCHO BIADOU CHLOE GABRIELLE <nguetchochloegabrielle@gmail.com>
2026-03-01 22:31:03 +0100

    fix: failed to fetch

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,18 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "**" },
     ],
   },
+  // Ajout du Proxy
+  async rewrites() {
+    // URL de ton backend (production ou dev)
+    const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || 'https://wakagenda-backend.onrender.com';
+    
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
